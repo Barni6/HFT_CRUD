cc16d7bec4d3b1afd69fad2e091e3bc06d29dd21
Barni6 <bszombat@gmail.com>
2022-11-23 14:21:56 +0100

    TypeController added

--- KJWTMR_HFT_2022231.Endpoint/Controllers/TypeController.cs
+++ KJWTMR_HFT_2022231.Endpoint/Controllers/TypeController.cs
@@ -1,6 +1,49 @@
-﻿namespace KJWTMR_HFT_2022231.Endpoint.Controllers
+﻿using KJWTMR_HTF_2022231.Models.Interfaces;
+using KJWTMR_HTF_2022231.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace KJWTMR_HFT_2022231.Endpoint.Controllers
 {
-    public class TypeController
+    [Route("[controller]")]
+    [ApiController]
+    public class TypeController : ControllerBase
     {
+        IBeerShopLogic<Type> typeLogic;
+
+        public TypeController(IBeerShopLogic<Type> typeLogic)
+        {
+            this.typeLogic = typeLogic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Type> ReadAll()
+        {
+            return typeLogic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Type Read(int id)
+        {
+            return this.typeLogic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Type value)
+        {
+            this.typeLogic.Create(value);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] Type value)
+        {
+            this.typeLogic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.typeLogic.Delete(id);
+        }
     }
 }
